743a1405711158c29cafe7cda17df1f3ea30d60e
subash kc <subaazkc@gmail.com>
2021-06-23 23:14:29 +0545

    widget

--- lib/Questions.dart
+++ lib/Questions.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
   // const ({ Key? key }) : super(key: key);
-  String questionText;
+  final String questionText;
 
   Question(this.questionText);
 
