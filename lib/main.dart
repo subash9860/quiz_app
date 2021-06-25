72ed90575a0f1373aa6dda4175ca0640cf65b3e8
subash kc <subaazkc@gmail.com>
2021-06-25 12:14:04 +0545

    debugsBanner:False

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quiz App"),
